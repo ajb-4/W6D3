146726cca6787a40341b3513eccf92b07e8a9b1c
ajb-4 <alexbrown4711@gmail.com>
2023-08-02 17:29:35 -0400

    Done with Project 1

--- practice-for-ch-rails-first-routes-long-practice-main/first_routes/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
+  # get 'chirps/:id', to: 'chirps#show'
+  # post 'chirps', to: 'chirps#create'
+  # get 'chirps', to: 'chirps#index'
+  # patch 'chirps/:id', to: 'chirps#update'
+  # put 'chirps/:id', to: 'chirps#update'
+  # get 'chirps/new', to: 'chirps#new'
+  # delete 'chirps/:id', to: 'chirps#destroy'
+  # get 'chirps/:id/edit', to: 'chirps#edit'
   # Defines the root path route ("/")
   # root "articles#index"
 end
